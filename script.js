da5ea80e25aae4a0f4ccb442704b4d01c5059d9c
phanahaile <phanahaile19@gmail.com>
2026-07-07 01:04:32 +0300

    Complete Rock Paper Scissors project

---
+++ script.js
@@ -0,0 +1,44 @@
+
+function getComputerChoice() {
+    let randomNumber = Math.random();
+    let computerChoice;
+
+    if (randomNumber < 0.33) {
+        computerChoice = "rock";
+    } else if (randomNumber < 0.66) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+
+    return computerChoice;
+}
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        console.log("Draw!");
+    } else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+        console.log("Player Wins!");
+    } else {
+        console.log("Computer Wins!");
+    }
+}
+
+function playGame() {
+    let playerChoice = prompt("Choose Rock, Paper or Scissors");
+playerChoice = playerChoice.toLowerCase();
+
+
+    let computerChoice = getComputerChoice();
+
+    console.log("Player:", playerChoice);
+    console.log("Computer:", computerChoice);
+
+    playRound(playerChoice, computerChoice);
+
+}
+playGame();
